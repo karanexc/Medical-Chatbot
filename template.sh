d6caa15a4c7ea516877369ec2f75bcc89111ee11
Karan Mhaswadkar <karan.mhaswadkar715@gmail.com>
2025-10-28 18:17:52 +0000

    Folder structure created

---
+++ template.sh
@@ -0,0 +1,15 @@
+# To create directories for a new project
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch requirements.txt
+
+echo "Project structure created successfully."
